f0bfebdb03bf3b4dfb51b662f7f2dc41a8241df4
Joan Perals <joan.perals@ixds.com>
2015-01-22 19:42:53 +0100

    Enable functionality for ng-repeat in vertical scrolling

--- src/slyDrtv.js
+++ src/slyDrtv.js
@@ -127,30 +127,35 @@ angularSly.directive('slyHorizontalRepeat',  function($timeout){
 });
 
 
-angularSly.directive('slyVerticalRepeat', function(){
+angularSly.directive('slyVerticalRepeat', function($timeout){
 	return {
 		restrict: 'A',
 		link: function (scope, el, attrs){
-			var frame = $(el);
-			var wrap  = $(el[0]).parent();
-			defaultOptions.horizontal = 0;
+            if (scope.$last === true) {
+                $timeout(function() {
+                    var frame = $(el[0]).parent().parent();
+                    var wrap = $(el[0]).parent().parent().parent();
 
-			var defaultControls = {
-				scrollBar: wrap.find('.scrollbar') || null,
-				pagesBar: wrap.find('.pages') || null,
-				forward: wrap.find('.forward') || null,
-				backward: wrap.find('.backward') || null,
-				prev: wrap.find('.prev') || null,
-				next: wrap.find('.next') || null,
-				prevPage: wrap.find('.prevPage') || null,
-				nextPage: wrap.find('.nextPage') || null
-			};
-			
-			// Merge parts into options object for sly argument
-			var options =  $.extend({}, defaultOptions, defaultControls, scope.$eval(attrs.slyOptions));
-			var callback = scope.$eval(attrs.slyCallback) || function(cb){};
-			// Call Sly on frame
-			frame.sly(options, callback());
+                    defaultOptions.horizontal = 0;
+
+                    var defaultControls = {
+                        scrollBar: wrap.find('.scrollbar') || null,
+                        pagesBar: wrap.find('.pages') || null,
+                        forward: wrap.find('.forward') || null,
+                        backward: wrap.find('.backward') || null,
+                        prev: wrap.find('.prev') || null,
+                        next: wrap.find('.next') || null,
+                        prevPage: wrap.find('.prevPage') || null,
+                        nextPage: wrap.find('.nextPage') || null
+                    };
+
+                    // Merge parts into options object for sly argument
+                    var options = $.extend({}, defaultOptions, defaultControls, scope.$eval(attrs.slyOptions));
+                    var callback = scope.$eval(attrs.slyCallback) || function(cb){};
+                    // Call Sly on frame
+                    frame.sly(options, callback());
+                });
+            }
 		}
 	}
 });
